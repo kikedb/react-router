5b5ffac36bb7d91e9ab502a4ccb8e5ccb7a69e41
kikedb <enriquediazbarrios@gmail.com>
2021-02-10 11:47:09 -0300

    Navegando a traves de la app con history

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Prueba extends Component {
+    handleClick = () => {
+        // console.log(this.props)
+        const { history } = this.props
+        history.push('/invoices')
+    }
     render() {
-        return 'Este es el componente de prueba'
+        return (
+            <div onClick={this.handleClick}>Este es el componente de prueba!</div>
+        )
     }
 }
